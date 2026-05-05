0536863318f7dce8c7af4103f4d344faffc91001
Sanan Malik <iamsanan50@gmail.com>
2026-05-05 20:58:29 +0500

    Chnages

--- ModelsClass/Controllers/HomeController.cs
+++ ModelsClass/Controllers/HomeController.cs
@@ -1,62 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using ModelsClass.Models;
-using ModelsClass.Models.Repository;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace ModelsClass.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly StudentRepository _studentrepository;
+       //private readonly StudentRepository _studentrepository;
         public IActionResult Index()
         {
 
 
-            ////var student = new List<ModelClass>
-            ////{
-            ////    new ModelClass
-            ////    {
-            ////        Name = "Sanan Malik",
-            ////        Degree = "BSCS",
-            ////        University = "MNSUAM",
-            ////        GraduationYear = 2028,
-            ////        GraduationMonth = 6
-            ////    },
-            ////     new ModelClass
-            ////    {
-            ////        Name = "Naveed Malik",
-            ////        Degree = "BSCS",
-            ////        University = "MNSUAM",
-            ////        GraduationYear = 2028,
-            ////        GraduationMonth = 6
-            ////    },
-            ////      new ModelClass
-            ////    {
-            ////        Name = "Abdullah Malik",
-            ////        Degree = "BSCS",
-            ////        University = "MNSUAM",
-            ////        GraduationYear = 2028,
-            ////        GraduationMonth = 6
-            ////    }
-            ////};
+            var  student = new List<ModelClass>
+            {
+                new ModelClass
+                {
+                    Name = "Sanan Malik",
+                    Degree = "BSCS",
+                    University = "MNSUAM",
+                    GraduationYear = 2028,
+                    GraduationMonth = 6
+                },
+                 new ModelClass
+                {
+                    Name = "Naveed Malik",
+                    Degree = "BSCS",
+                    University = "MNSUAM",
+                    GraduationYear = 2028,
+                    GraduationMonth = 6
+                },
+                  new ModelClass
+                {
+                    Name = "Abdullah Malik",
+                    Degree = "BSCS",
+                    University = "MNSUAM",
+                    GraduationYear = 2028,
+                    GraduationMonth = 6
 
-            ////ViewData["_students"] = student;
+                } 
+            };
+
+            ViewData["_students"] = student;
             return View();
 
         }
-        public HomeController(StudentRepository studentrepository)
-        {
-            _studentrepository = new StudentRepository();
-        }
-        public List<ModelClass> GetStudents()
-        {
+        //public HomeController(StudentRepository studentrepository)
+        //{
+        //    _studentrepository = new StudentRepository();
+        //}
+        //public List<ModelClass> GetStudents()
+        //{
             
-            return _studentrepository.GetStudents();
-        }
-        public ModelClass  GetStudentById(int id)
-        {
-            return _studentrepository.GetStudentById(id);
-        }
+        //    return _studentrepository.GetStudents();
+        //}
+        //public ModelClass  GetStudentById(int id)
+        //{
+        //    return _studentrepository.GetStudentById(id);
+        //}
 
         public IActionResult Privacy()
         {
--- ModelsClass/Models/Repository/StudentRepository.cs
+++ ModelsClass/Models/Repository/StudentRepository.cs
@@ -1,51 +1,51 @@
-﻿using ModelsClass.Models.Repository;
-namespace ModelsClass.Models.Repository
-{
-    public class StudentRepository : IStudent
-    {
-        public ModelClass GetStudentById(int id)
-        {
-            return DataSource().Where(student => student.Id == id).FirstOrDefault();
-        }
+﻿//using ModelsClass.Models.Repository;
+//namespace ModelsClass.Models.Repository
+//{
+//    public class StudentRepository : IStudent
+//    {
+//        public ModelClass GetStudentById(int id)
+//        {
+//            return DataSource().Where(student => student.Id == id).FirstOrDefault();
+//        }
 
-        public List<ModelClass> GetStudents()
-        {
-            return DataSource();
-        }
-        private List<ModelClass> DataSource()
-        {
-            {
-                return new List<ModelClass>
-            {
-                new ModelClass
-                {
-                    Id = 1,
-                   Name = "Sanan Malik",
-                   Degree = "BSCS",
-                  University = "MNSUAM",
-                   GraduationYear = 2028,
-                  GraduationMonth = 6
-                },
-                new ModelClass
-               {
-                    Id = 2,
-                   Name = "Naveed Malik",
-                   Degree = "BSCS",
-                   University = "MNSUAM",
-                   GraduationYear = 2028,
-                   GraduationMonth = 6
-                },
-                  new ModelClass
-                {
-                      Id = 3,
-                    Name = "Abdullah Malik",
-                   Degree = "BSCS",
-                   University = "MNSUAM",
-                   GraduationYear = 2028,
-                   GraduationMonth = 6
-                  }
-            };
-            }
-        }
-    }
-}
+//        public List<ModelClass> GetStudents()
+//        {
+//            return DataSource();
+//        }
+//        private List<ModelClass> DataSource()
+//        {
+//            {
+//                return new List<ModelClass>
+//            {
+//                new ModelClass
+//                {
+//                    Id = 1,
+//                   Name = "Sanan Malik",
+//                   Degree = "BSCS",
+//                  University = "MNSUAM",
+//                   GraduationYear = 2028,
+//                  GraduationMonth = 6
+//                },
+//                new ModelClass
+//               {
+//                    Id = 2,
+//                   Name = "Naveed Malik",
+//                   Degree = "BSCS",
+//                   University = "MNSUAM",
+//                   GraduationYear = 2028,
+//                   GraduationMonth = 6
+//                },
+//                  new ModelClass
+//                {
+//                      Id = 3,
+//                    Name = "Abdullah Malik",
+//                   Degree = "BSCS",
+//                   University = "MNSUAM",
+//                   GraduationYear = 2028,
+//                   GraduationMonth = 6
+//                  }
+//            };
+//            }
+//        }
+//    }
+//}
